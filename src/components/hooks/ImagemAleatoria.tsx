65507869b49f04495a2a92afdf61184d538e85b3
Lafaiete do Vale <lafa.vale@gmail.com>
2023-05-03 23:25:23 -0300

    Stated component

--- src/components/hooks/ImagemAleatoria.tsx
+++ src/components/hooks/ImagemAleatoria.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
+import { useState } from "react";
 
 export default function ImagemAleatoria() {
-  const url = "https://source.unsplash.com/featured/300x300?";
+  const [pesquisa, setPesquisa] = useState<string>("abstract");
+  const [tamanho, setTamanho] = useState<number>(300);
+
+  const url = "https://source.unsplash.com/featured/";
 
   function urlImagem() {
     return `${url}${pesquisa}`;
   }
-
-  let pesquisa: string = "";
 
   function renderizarBotao(valor: string) {
     return (
@@ -16,7 +18,7 @@ export default function ImagemAleatoria() {
           bg-blue-500 px-4 py-2 rounded-md
     `}
         onClick={() => {
-          pesquisa = valor;
+          setPesquisa(valor);
           console.log(urlImagem());
         }}
       >
@@ -27,11 +29,33 @@ export default function ImagemAleatoria() {
 
   return (
     <div className='flex flex-col gap-3 border border-zinc-500 p-7 rounded-md'>
-      <Image src={urlImagem()} height={300} width={300} alt='image' />
+      <div className='flex justify-center gap-7 mb-5'>
+        <span>{pesquisa}</span>
+        <span>
+          {tamanho}x{tamanho}px
+        </span>
+      </div>
+      <Image
+        src={`${url}${tamanho}x${tamanho}?${pesquisa}`}
+        height={300}
+        width={300}
+        alt='Imagem'
+        className='rounded-md'
+      />
       <div className='flex justify-between gap-5'>
         {renderizarBotao("abstract")}
         {renderizarBotao("city")}
         {renderizarBotao("person")}
+      </div>
+      <div>
+        <input
+          type='number'
+          value={tamanho}
+          className='bg-zinc-800 p-2 rounded-md outline-none w-full'
+          onChange={(e) => {
+            setTamanho(Number(e.target.value));
+          }}
+        />
       </div>
     </div>
   );
